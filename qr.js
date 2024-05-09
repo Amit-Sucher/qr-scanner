de67433ec66b5df20cee140298c7d03bd2b56791
AmitSu <sucher.amit@gmail.com>
2024-05-09 18:51:24 +0300

    added s to http

--- qr.js
+++ qr.js
@@ -21,7 +21,7 @@ function createNotification(messagetext = message) {
 
 function fetchRequest(formData, file){
     infoText.innerText = "Scanning QR Code..."
-    fetch("http://api.qrserver.com/v1/read-qr-code/",{
+    fetch("https://api.qrserver.com/v1/read-qr-code/",{
         method: "POST", body: formData
     }).then(res => res.json()).then(result =>{
         result = result[0].symbol[0].data
